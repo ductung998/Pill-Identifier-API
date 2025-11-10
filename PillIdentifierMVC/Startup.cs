0f78aee89d4ccc31ab1b19d0fe1306a562ac9274
Nguyễn Đức Tùng <ductung998@gmail.com>
2025-11-10 09:37:44 +0700

    Add MVC project
    Blank MVC

---
+++ PillIdentifierMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PillIdentifierMVC.Startup))]
+namespace PillIdentifierMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
